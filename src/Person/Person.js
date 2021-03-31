634a11d4f7e342e0b3ceff7665d69ed15ed7b771
bstali <bstali56@gmail.com>
2021-03-31 20:33:46 +0500

    add css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.module.css';
 
 const person = props =>{
 
     return(
-    <div className="Person" >
+    <div className={classes.Person} >
        <h2 onClick={props.click}>My name is {props.name}. I'm a Person and my age is: {props.age}</h2>
        <h2>{props.children}</h2>
        <input type='text' onChange={props.changed} value={props.name}/>
